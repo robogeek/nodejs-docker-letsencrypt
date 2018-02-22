34d2207bd7b9e45d8cb7eb6007a803dfd7523017
David Herron <david@davidherron.com>
2018-02-21 21:24:32 -0800

    Initial revision

---
+++ app/app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+// const forceSSL = require('express-force-ssl');
+const path = require('path');
+const util = require('util');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const index = require('./routes/index');
+const users = require('./routes/users');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+// app.use(forceSSL);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error(`Not Found requrl=${req.path} query=${util.inspect(req.query)}`);
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  console.error(err.stack);
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+console.log(`Launching app server`);
+
+module.exports = app;
